c207aa2ea8fae15a4198d37db02b64247d717c33
gleepgorp <staphmaster2002@gmail.com>
2024-12-05 14:17:40 +0800

    scaffold basic ui and mockdata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,24 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-slate-400/50 border-b">
+      <div>Gallery</div>
+      <div>Sign in</div>
+    </nav>
+  )
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
 import Link from "next/link";
 
 export default function HomePage() {
+  const mockURLS = [
+    "https://utfs.io/f/XWqGMo5TBKc2d7FQLfIYSfveCTNjdQt7Awk8lGZgphXVBmnE",
+    "https://utfs.io/f/XWqGMo5TBKc2d7FQLfIYSfveCTNjdQt7Awk8lGZgphXVBmnE",
+    "https://utfs.io/f/XWqGMo5TBKc2vfNttSFJEu52mVcfP49g7Yz1rGdwiWsnHSUo",
+    "https://utfs.io/f/XWqGMo5TBKc2jEsI5Tteq0ZbskBl3FwuQrJhznAEm8DcL6gf",
+    "https://utfs.io/f/XWqGMo5TBKc261OF9Jn7yaONS8bILrVW2xkpMgBw14AY5CUP"
+  ];
+
+  const mockImages = mockURLS.map((url, index) => ({
+    id: index + 1,
+    url,
+  }));
+
+  const mockImagesComponent = [...mockImages, ...mockImages, ...mockImages].map((image) => {
+    return (
+      <div key={image.id} className="">
+        <img src={image.url} alt="image" className="max-w-40 max-w-40 h-40 w-40 object-cover"/>
+      </div>
+    )
+  })
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello gallery!
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImagesComponent}
+      </div>
     </main>
   );
 }
